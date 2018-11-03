9876ffcd58461f07210762dc44a41c17deeb4355
ice_iix <ice_ix@protonmail.ch>
2018-11-03 15:05:06 -0700

    Get the bigints for modulus and exponent of public key from ASN.1

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use openssl::rand::rand_bytes;
 use openssl::rsa::{Rsa, Padding};
 
 extern crate num;
-use num::bigint::BigUint;
+use num::bigint::{BigInt};
+use num::traits::Zero;
 
 extern crate simple_asn1;
 use simple_asn1::{from_der, ASN1Block};
@@ -29,6 +30,7 @@ fn main() {
    */
     let packet_public_key_data = [48, 129, 159, 48, 13, 6, 9, 42, 134, 72, 134, 247, 13, 1, 1, 1, 5, 0, 3, 129, 141, 0, 48, 129, 137, 2, 129, 129, 0, 149, 92, 126, 71, 214, 186, 100, 139, 40, 104, 65, 254, 200, 105, 71, 66, 241, 84, 172, 206, 206, 217, 49, 214, 16, 50, 6, 234, 97, 21, 170, 139, 234, 88, 220, 105, 27, 115, 56, 103, 53, 234, 84, 255, 129, 147, 41, 146, 68, 39, 120, 208, 141, 142, 39, 242, 182, 97, 4, 204, 236, 190, 104, 101, 234, 46, 71, 248, 55, 88, 213, 56, 145, 154, 142, 184, 144, 55, 105, 241, 179, 205, 174, 107, 40, 77, 46, 201, 197, 51, 20, 246, 95, 207, 227, 5, 210, 42, 107, 135, 219, 126, 207, 216, 181, 2, 130, 57, 203, 239, 232, 68, 220, 131, 211, 86, 168, 125, 193, 91, 148, 153, 109, 76, 109, 50, 2, 139, 2, 3, 1, 0, 1];
 
+    // Outer ASN.1 encodes 1.2.840.113549.1.1 OID and wraps a bitstring, find it
     let asns: Vec<ASN1Block> = from_der(&packet_public_key_data).unwrap();
     println!("asns = {:?}", asns);
     for asn in asns.iter() {
@@ -36,11 +38,34 @@ fn main() {
     }
     let mut result: Vec<Vec<u8>> = vec![];
     find_bitstrings(asns, &mut result);
-    let modulus_bytes = &result[0];
+
+    let inner_asn: Vec<ASN1Block> = from_der(&result[0]).unwrap();
+    println!("inner_asn {:?}", inner_asn[0]);
+    match &inner_asn[0] {
+        ASN1Block::Sequence(_, _, blocks) => {
+            let zero = Zero::zero();
+            let n: &BigInt = match &blocks[0] {
+                ASN1Block::Integer(_, _, n) => n,
+                _ => &zero,
+            };
+
+            let e: &BigInt = match &blocks[1] {
+                ASN1Block::Integer(_, _, n) => n,
+                _ => &zero,
+            };
+
+            println!("N={:?}\ne={:?}", n, e);
+        },
+        _ => (),
+    }
+
+/*
     println!("modulus_bytes = {:?}", modulus_bytes);
     // TODO: ASN.1 BitString to bignum?
     let modulus = BigUint::from_bytes_be(&modulus_bytes);
     println!("modulus = {:}", modulus);
+    */
+
 
     let rsa = Rsa::public_key_from_der(&packet_public_key_data).unwrap();
     let mut shared = [0; 16];
