9d4bc304014af05d8ea370646f3a11fd1a99c8cc
ice_iix <ice_ix@protonmail.ch>
2018-11-03 13:08:34 -0700

    Parse ASN.1 DER with simple-asn1, find the BitString

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,18 @@ use openssl::rand::rand_bytes;
 use openssl::rsa::{Rsa, Padding};
 
 extern crate num;
+extern crate simple_asn1;
+use simple_asn1::{from_der, ASN1Block};
+
+fn find_bitstrings(asns: Vec<ASN1Block>, mut result: &mut Vec<Vec<u8>>) {
+    for asn in asns.iter() {
+        match asn {
+            ASN1Block::BitString(_, _, _, bytes) => result.push(bytes.to_vec()),
+            ASN1Block::Sequence(_, _,  blocks) => find_bitstrings(blocks.to_vec(), &mut result),
+            _ => (),
+        }
+    }
+}
 
 fn main() {
     /*
@@ -14,6 +26,15 @@ fn main() {
    18:d=1  hl=3 l= 141 prim: BIT STRING
    */
     let packet_public_key_data = [48, 129, 159, 48, 13, 6, 9, 42, 134, 72, 134, 247, 13, 1, 1, 1, 5, 0, 3, 129, 141, 0, 48, 129, 137, 2, 129, 129, 0, 149, 92, 126, 71, 214, 186, 100, 139, 40, 104, 65, 254, 200, 105, 71, 66, 241, 84, 172, 206, 206, 217, 49, 214, 16, 50, 6, 234, 97, 21, 170, 139, 234, 88, 220, 105, 27, 115, 56, 103, 53, 234, 84, 255, 129, 147, 41, 146, 68, 39, 120, 208, 141, 142, 39, 242, 182, 97, 4, 204, 236, 190, 104, 101, 234, 46, 71, 248, 55, 88, 213, 56, 145, 154, 142, 184, 144, 55, 105, 241, 179, 205, 174, 107, 40, 77, 46, 201, 197, 51, 20, 246, 95, 207, 227, 5, 210, 42, 107, 135, 219, 126, 207, 216, 181, 2, 130, 57, 203, 239, 232, 68, 220, 131, 211, 86, 168, 125, 193, 91, 148, 153, 109, 76, 109, 50, 2, 139, 2, 3, 1, 0, 1];
+
+    let asns: Vec<ASN1Block> = from_der(&packet_public_key_data).unwrap();
+    println!("asns = {:?}", asns);
+    for asn in asns.iter() {
+        println!("asn = {:?}", asn);
+    }
+    let mut result: Vec<Vec<u8>> = vec![];
+    find_bitstrings(asns, &mut result);
+    println!("result[0] = {:?}", result[0]);
 
     let rsa = Rsa::public_key_from_der(&packet_public_key_data).unwrap();
     /*
